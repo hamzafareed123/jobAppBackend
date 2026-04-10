5be9c33ba1c697fda125e4aecf3b4fad15bb2771
Hamza <hamzafareed616@gmail.com>
2026-04-10 14:37:24 +0500

    dashboard data added

--- Backend/src/config/db.ts
+++ Backend/src/config/db.ts
@@ -1,19 +1,15 @@
 import mongoose from "mongoose";
 import { ENV } from "./env";
 
-
-export const dbConnect=async()=>{
-    try {
-        if (!ENV.MONGO_URL) {
-
-            throw new Error("MONGO_URL not set in environment variables");
-        }
-         console.log("Connecting with URL:", ENV.MONGO_URL);
-        await mongoose.connect(ENV.MONGO_URL)
-        console.log(" Database Connected Successfully");
-
-    } catch (error) {
-        console.error("Error in connecting Database:", error)        
+export const dbConnect = async () => {
+  try {
+    if (!ENV.MONGO_URL) {
+      throw new Error("MONGO_URL not set in environment variables");
     }
-}
 
+    await mongoose.connect(ENV.MONGO_URL);
+    console.log(" Database Connected Successfully");
+  } catch (error) {
+    console.error("Error in connecting Database:", error);
+  }
+};
